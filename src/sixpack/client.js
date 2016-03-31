90640bc2ca74115f8498f01e508ea05f0faec1de
xcv58 <chenyihonglove@gmail.com>
2016-03-31 10:18:29 -0400

    add convert for Sixpack

--- src/sixpack/client.js
+++ src/sixpack/client.js
@@ -8,16 +8,22 @@ class ClientComponent extends React.Component {
   }
 
   participate({id, experiment}) {
-    const session = new sixpack.Session({
+    this.session = new sixpack.Session({
       client_id: id,
-      base_url: 'http://do.xcv58.com:5000'
+      base_url: 'http://localhost:5000'
+      // base_url: 'http://do.xcv58.com:5000'
     });
-    session.participate(experiment, ['red', 'blue', 'green'], (err, res) => {
+    this.session.participate(experiment, ['red', 'blue', 'green'], (err, res) => {
       if (err) throw err;
       const color = res.alternative.name
       console.log(experiment, color);
       this.setState({color});
     });
+  }
+
+  convert() {
+    const {id, experiment} = this.props;
+    this.session.convert(experiment);
   }
 
   componentDidMount() {
@@ -38,7 +44,9 @@ class ClientComponent extends React.Component {
           Exp: {experiment}, Client ID: {id}
         </h2>
         <div>
-          {color}
+          {color} <button onClick={this.convert.bind(this)}>
+            Convert
+          </button>
         </div>
         <hr />
       </div>
